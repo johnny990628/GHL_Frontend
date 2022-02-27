90aece571047c56e71c4fe41315961343e221c54
johnny990628 <johnny99457@gmail.com>
2022-02-28 00:41:54 +0800

    feat:custom table toolbar

--- src/Components/CustomTable/CustomTable.js
+++ src/Components/CustomTable/CustomTable.js
@@ -1,18 +1,39 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Box } from '@mui/material';
-import { DataGrid } from '@mui/x-data-grid';
+import {
+    DataGrid,
+    GridToolbarContainer,
+    GridToolbarColumnsButton,
+    GridToolbarFilterButton,
+    GridToolbarDensitySelector,
+    GridToolbarExport,
+} from '@mui/x-data-grid';
 import useStyles from './Style';
 
+const CustomToolbar = () => {
+    return (
+        <GridToolbarContainer>
+            <GridToolbarColumnsButton />
+            <GridToolbarFilterButton />
+            <GridToolbarDensitySelector />
+            <GridToolbarExport csvOptions={{ fileName: '診所檔案', utf8WithBom: true }} />
+        </GridToolbarContainer>
+    );
+};
+
 const CustomTable = ({ rows, columns }) => {
+    const [pageSize, setPageSize] = useState(5);
     const classes = useStyles();
     return (
         <Box className={classes.container}>
             <DataGrid
                 rows={rows}
                 columns={columns}
-                pageSize={9}
-                autoHeight
+                pageSize={pageSize}
+                onPageSizeChange={(newPageSize) => setPageSize(newPageSize)}
+                rowsPerPageOptions={[5, 10, 20]}
                 checkboxSelection
+                components={{ Toolbar: CustomToolbar }}
                 sx={{ fontSize: '1.3rem', backgroundColor: '#F5D7DE', p: 2, borderRadius: '1rem' }}
             />
         </Box>
--- src/Components/CustomTable/Style.js
+++ src/Components/CustomTable/Style.js
@@ -4,6 +4,7 @@ const useStyles = makeStyles((theme) => ({
     container: {
         margin: theme.spacing(0.4),
         width: '86%',
+        height: '66vh',
         boxShadow: '6px 6px 10px rgba(0,0,0,0.2)',
     },
 }));
