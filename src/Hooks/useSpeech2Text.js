05156ab03aa9c6fb5738914d9e5e1bb9ed46f4fc
johnny990628 <johnny99457@gamil.com>
2022-07-14 18:20:16 +0800

    refactor: change react speech recognition to web speech api

--- src/Hooks/useSpeech2Text.js
+++ src/Hooks/useSpeech2Text.js
@@ -1,26 +1,56 @@
 import React, { useEffect, useState } from 'react'
-import SpeechRecognition, { useSpeechRecognition } from 'react-speech-recognition'
+import { useSpeechRecognition } from 'react-speech-recognition'
 import { useDebouncedCallback } from 'use-debounce'
 
 const useSpeech2Text = () => {
     const [record, setRecord] = useState(false)
-    const { transcript, listening, resetTranscript, browserSupportsSpeechRecognition } = useSpeechRecognition()
+    const [listening, setListening] = useState(false)
+    const [transcript, setTranscript] = useState('')
+    // const { transcript, listening, resetTranscript, browserSupportsSpeechRecognition } = useSpeechRecognition()
+    let SpeechRecognition = window.webkitSpeechRecognition || window.SpeechRecognition
+    let SpeechGrammarList = window.SpeechGrammarList || window.webkitSpeechGrammarList
+    let recognition = new SpeechRecognition()
+    let speechRecognitionList = new SpeechGrammarList()
+    let moods = ['左葉', '右葉', '左腎', '右腎']
+    let grammar = '#JSGF V1.0; grammar moods; public <moods> = ' + moods.join(' | ') + ';'
+    speechRecognitionList.addFromString(grammar, 1)
 
-    const stop = useDebouncedCallback(() => {
-        resetTranscript()
-    }, 500)
+    recognition.grammars = speechRecognitionList
+    recognition.lang = 'zh-TW'
+    recognition.continuous = true
+    recognition.interimResults = false
+    recognition.maxAlternatives = 1
 
     useEffect(() => {
-        if (transcript) stop()
-    }, [transcript])
-
-    useEffect(() => {
-        if (record) SpeechRecognition.startListening({ continuous: true, language: 'zh-TW' })
-        else {
-            SpeechRecognition.stopListening()
-            resetTranscript()
+        if (record) {
+            setListening(true)
+            recognition.start()
+            recognition.onresult = ({ results }) => {
+                const resultIndex = results.length - 1
+                setTranscript(results[resultIndex][0].transcript)
+            }
+        } else {
+            recognition.stop()
+            setListening(false)
+            setTranscript('')
         }
     }, [record])
+
+    // const stop = useDebouncedCallback(() => {
+    //     resetTranscript()
+    // }, 500)
+
+    // useEffect(() => {
+    //     if (transcript) stop()
+    // }, [transcript])
+
+    // useEffect(() => {
+    //     if (record) SpeechRecognition.startListening({ continuous: true, language: 'zh-TW' })
+    //     else {
+    //         SpeechRecognition.stopListening()
+    //         resetTranscript()
+    //     }
+    // }, [record])
 
     return { transcript, setRecord, listening }
 }
