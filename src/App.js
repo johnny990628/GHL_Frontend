15250024384b884c9068501f436d98a60ba3dce9
Louis <81738019+Yang-Jiaxiang@users.noreply.github.com>
2022-07-27 23:29:32 +0800

    fix：patientform page verify

--- src/App.js
+++ src/App.js
@@ -1,27 +1,48 @@
-import React, { useEffect } from 'react'
-import { useDispatch, useSelector } from 'react-redux'
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { HashRouter, useLocation, Routes, Route } from "react-router-dom";
 
-import Layout from './Components/Layout/Layout'
-import Login from './Pages/Login/Login'
-import { apiVerify } from './Axios/Auth'
-import { fillAuthState } from './Redux/Slices/Auth'
-import CustomAlert from './Components/CustomAlert/CustomAlert'
+import Layout from "./Components/Layout/Layout";
+import Login from "./Pages/Login/Login";
+import { apiVerify } from "./Axios/Auth";
+import { fillAuthState } from "./Redux/Slices/Auth";
+import CustomAlert from "./Components/CustomAlert/CustomAlert";
+import PatientForm from "./Pages/PatientForm";
 
 const App = () => {
-    const dispatch = useDispatch()
-    const { verify } = useSelector(state => state.auth)
-    const isLoggedIn = localStorage.getItem('isLoggedIn')
+    const dispatch = useDispatch();
+    const { verify } = useSelector((state) => state.auth);
+    const isLoggedIn = localStorage.getItem("isLoggedIn");
 
     useEffect(() => {
-        if (isLoggedIn) apiVerify().then(res => dispatch(fillAuthState({ user: res.data.user, token: res.data.token })))
-    }, [])
+        if (isLoggedIn)
+            apiVerify().then((res) =>
+                dispatch(
+                    fillAuthState({
+                        user: res.data.user,
+                        token: res.data.token,
+                    })
+                )
+            );
+    }, []);
 
     return (
         <>
-            {verify ? <Layout /> : <Login />}
+            {verify ? <Layout /> : <NotLoginLayOut />}
             <CustomAlert />
         </>
-    )
-}
+    );
+};
 
-export default App
+const NotLoginLayOut = () => {
+    return (
+        <HashRouter>
+            <Routes>
+                <Route path="*" element={<Login />} />
+                <Route path="/patientform" element={<PatientForm />} />
+            </Routes>
+        </HashRouter>
+    );
+};
+
+export default App;
