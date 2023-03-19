24b3b525c3d0689ed275d0b506ba9d21286c150d
johnny99457 <johnny99457@gmail.com>
2023-03-19 13:35:58 +0800

    Feat : add dicom slice

--- src/Pages/Image/Image.js
+++ src/Pages/Image/Image.js
@@ -14,54 +14,12 @@ import { openAlert } from '../../Redux/Slices/Alert'
 import { activeDepartment, createDepartment, deleteDepartment, fetchDepartment } from '../../Redux/Slices/Department'
 
 const Image = () => {
-    const [name, setName] = useState('')
-    const [address, setAddress] = useState('')
-    const [activeSwitch, setActiveSwitch] = useState(true)
-    const [errorField, setErrorField] = useState([])
     const dispatch = useDispatch()
     const classes = useStyles()
     const { results, count, page, loading } = useSelector(state => state.department)
 
     const fetchData = async params => {
         dispatch(fetchDepartment(params))
-    }
-
-    const hasEmptyField = () => {
-        const errorFieldList = Object.entries({ name, address })
-            .map(([key, value]) => !value && key)
-            .filter(key => key)
-        setErrorField(errorFieldList)
-        return errorFieldList.length !== 0
-    }
-
-    const handleHelperText = fieldName => {
-        switch (fieldName) {
-            case 'name':
-                return results.find(d => d.name === name) && '此部門已存在'
-            default:
-                return ''
-        }
-    }
-
-    const handleChange = useDebouncedCallback((value, name) => {
-        switch (name) {
-            case 'name':
-                setName(value)
-                break
-            case 'address':
-                setAddress(value)
-                break
-
-            default:
-                break
-        }
-    }, 500)
-
-    const handleSubmit = async () => {
-        if (hasEmptyField()) return
-        dispatch(createDepartment({ name, address, active: activeSwitch }))
-        setName('')
-        setAddress('')
     }
 
     const columns = useMemo(
@@ -112,54 +70,8 @@ const Image = () => {
         []
     )
 
-    const inputModel = [
-        { name: 'name', label: '部門名稱', value: name, setValue: setName, required: true },
-        { name: 'address', label: '部門地址', value: address, setValue: setAddress, required: true },
-    ]
-
     return (
         <Box className={classes.container}>
-            <Accordion elevation={0} className={classes.accordion}>
-                <AccordionSummary expandIcon={<ArrowDropDown />} sx={{ flexDirection: 'column-reverse' }} />
-                <AccordionDetails>
-                    <Box className={classes.formWrapper}>
-                        <Box className={classes.formHeader}>新增部門</Box>
-                        <FormGroup>
-                            <FormControlLabel
-                                control={<Switch checked={activeSwitch} onChange={e => setActiveSwitch(e.target.checked)} />}
-                                label={<Box sx={{ fontSize: '1.4rem' }}>自動啟用部門</Box>}
-                            />
-                        </FormGroup>
-                        <Box className={classes.formContainer}>
-                            <Box className={classes.formBody}>
-                                {inputModel.map(({ name, label, value, setValue, required }) => (
-                                    <CustomInput
-                                        key={name}
-                                        name={name}
-                                        label={label}
-                                        value={value}
-                                        setValue={setValue}
-                                        handleChange={handleChange}
-                                        handleHelperText={handleHelperText}
-                                        error={errorField.includes(name)}
-                                        required={required}
-                                    />
-                                ))}
-                            </Box>
-                            <Button
-                                variant="contained"
-                                className={classes.button}
-                                fullWidth
-                                sx={{ fontSize: '1.1rem ', padding: '.5rem', margin: '1rem' }}
-                                onClick={handleSubmit}
-                            >
-                                新增
-                            </Button>
-                        </Box>
-                    </Box>
-                </AccordionDetails>
-            </Accordion>
-
             <CustomTable
                 columns={columns}
                 fetchData={fetchData}
---
+++ src/Redux/Slices/Dicom.js
@@ -0,0 +1,46 @@
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit'
+
+import { openAlert } from './Alert'
+import { tokenExpirationHandler } from '../../Utils/ErrorHandle'
+
+export const fetchDicom = createAsyncThunk('dicom/fetchDicom', async (params, thunkAPI) => {
+    try {
+        const response = await apiGetDepartments(params)
+        const { results, count } = response.data
+        return { results, count, page: Math.ceil(count / params.limit) }
+    } catch (e) {
+        thunkAPI.dispatch(tokenExpirationHandler(e.response))
+        return thunkAPI.rejectWithValue()
+    }
+})
+
+const initialState = { results: [], count: 0, page: 1, loading: false }
+const dicomSlice = createSlice({
+    name: 'dicom',
+    initialState,
+    reducers: {
+        dicomTrigger: (state, action) => ({ ...state, count: -1 }),
+    },
+    extraReducers: {
+        [fetchDicom.pending]: (state, action) => {
+            return {
+                ...state,
+                loading: true,
+            }
+        },
+        [fetchDicom.fulfilled]: (state, action) => {
+            return {
+                ...action.payload,
+                loading: false,
+            }
+        },
+        [fetchDicom.rejected]: (state, action) => {
+            return {
+                ...state,
+                loading: false,
+            }
+        },
+    },
+})
+export const { dicomTrigger } = dicomSlice.actions
+export default dicomSlice.reducer
